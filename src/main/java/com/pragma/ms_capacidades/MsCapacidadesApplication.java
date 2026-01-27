3c25c0f1c1b7cc559c57b9b42c3e00dcf76119ef
Wilson Bastidas <wilson.bastidas@pragma.com.co>
2026-01-27 08:44:04 -0500

    initial commit

---
+++ src/main/java/com/pragma/ms_capacidades/MsCapacidadesApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_capacidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCapacidadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCapacidadesApplication.class, args);
+    }
+
+}
